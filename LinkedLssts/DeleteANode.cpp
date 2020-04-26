9448a766bc64c4ea87d044196883d8854de1426f
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2020-04-26 22:18:48 +0530

    Create DeleteANode.cpp

---
+++ LinkedLssts/DeleteANode.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    if(head == NULL)
+        return head;
+    SinglyLinkedListNode* next = head->next;
+    if(position==0){
+        free(head);
+        return next;
+    }
+    SinglyLinkedListNode* prev = NULL;
+    SinglyLinkedListNode* temp = head;
+    int i =0;
+    while(temp && i<position){
+        i++;
+        prev = temp;
+        temp=temp->next;
+    }
+    prev->next = temp->next;
+    free(temp);
+    return head;
+}
+
+int main()
