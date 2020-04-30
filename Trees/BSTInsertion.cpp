624ce6499536a49b0d63f63c49d2f02258ff7477
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2020-04-30 15:39:46 +0530

    Create BSTInsertion.cpp

---
+++ Trees/BSTInsertion.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+        if(!root){
+            root = new Node(data);
+        }
+        else if(data>=root->data){
+            if(!root->right){
+                root->right = new Node(data);
+            }
+            else{
+                root->right = insert(root->right,data);
+            }
+        }
+        else {
+            if(!root->left){
+                root->left = new Node(data);
+            }
+            else{
+                root->left = insert(root->left,data);
+            }
+        }
+        return root;
+    }
+
+};
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+    myTree.preOrder(root);
+  
+    return 0;
+}
